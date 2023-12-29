51b589cd301c8cd930a1ae552724b7d3ee27ef71
vyankatesh2004 <vyankateshdnagargoje04@gmail.com>
2023-12-29 23:06:39 +0530

    some changes

--- server/index.js
+++ server/index.js
@@ -1,7 +1,37 @@
-const http = require('http');
-const app = require('./app');
-const port = process.env.PORT || 3000;
+const express = require('express')
+const cors = require('cors');
+const bodyParser = require("body-parser");
+const mongoose = require('mongoose');
+require('dotenv').config();
+const cookieParser = require("cookie-parser");
+const authRoute = require("./Routes/AuthRoute");
 
+const app = express()
+const MONGODB_URI = process.env.MONGODB_URI;
 
-const server = http.createServer(app);
-server.listen(port,()=>{console.log('this app is running on '+ port)});
+var jsonParser = bodyParser.json();
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
+
+app.use(
+  cors({
+    origin: ["https://leetcode-frontend-theta.vercel.app"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
+
+app.use(cors());
+app.use(jsonParser);
+app.use(cookieParser());
+
+mongoose.connect(MONGODB_URI);
+
+app.use('/', (req, res) => {
+  res.send(200).json({message: 'Hello'});
+})
+
+app.use ('/', authRoute);
+
+app.listen(PORT, () => {
+  console.log(`listening on port ${PORT}`);
+})
